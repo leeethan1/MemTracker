2fde8bbe994ace9c6ea2ba55fbfff750251efcfc
ethanlee <lee3159@purdue.edu>
2020-01-25 15:51:55 -0500

    added new stage

--- app/src/main/java/com/example/memtracker/Organizerpage.java
+++ app/src/main/java/com/example/memtracker/Organizerpage.java
@@ -10,5 +10,7 @@ public class Organizerpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizerpage);
+
     }
+
 }
