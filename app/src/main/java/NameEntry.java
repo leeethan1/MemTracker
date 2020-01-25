03b32c817532453c137f8869beef271cbc5073ff
nishantsuresh <nishantsureshwin@gmail.com>
2020-01-25 15:19:35 -0500

    My activities were put in here.

---
+++ app/src/main/java/NameEntry.java
@@ -0,0 +1,27 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NameEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_entry);
+    }
+
+
+
+    public void onClick(View view) {
+        Intent i = new Intent(NameEntry.this, organiserPage.class);
+        startActivity(i);
+
+        Intent b = new Intent(NameEntry.this, organiserPage.class);
+        EditText input = findViewById(R.id.Name);
+        b.putExtra("orgKey", input.getText().toString());
+        startActivity(b);
+    }
+}
